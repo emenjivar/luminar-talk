69286a89e20189eec27049df7184432ba7fc1e25
carlos menjivar <carlosmenjivar@ravn.co>
2024-02-25 20:32:24 -0600

    feat: Detect flashes from the camera.
    Print ON/OFF on screen according to the light state.

--- app/src/main/java/com/emenjivar/luminar/screen/camera/CustomImageAnalyzer.kt
+++ app/src/main/java/com/emenjivar/luminar/screen/camera/CustomImageAnalyzer.kt
@@ -6,23 +6,26 @@ import androidx.camera.core.ImageProxy
 import org.opencv.android.Utils
 import org.opencv.core.Core
 import org.opencv.core.Mat
-import org.opencv.core.MatOfPoint
+import org.opencv.core.MatOfKeyPoint
 import org.opencv.core.Point
 import org.opencv.core.Rect
 import org.opencv.core.Scalar
 import org.opencv.core.Size
+import org.opencv.features2d.Features2d
+import org.opencv.features2d.SimpleBlobDetector
+import org.opencv.features2d.SimpleBlobDetector_Params
 import org.opencv.imgproc.Imgproc
 import org.opencv.imgproc.Imgproc.FILLED
 import org.opencv.imgproc.Imgproc.FONT_HERSHEY_COMPLEX_SMALL
 import org.opencv.imgproc.Imgproc.THRESH_BINARY
 
-@androidx.annotation.OptIn(androidx.camera.core.ExperimentalGetImage::class)
 class CustomImageAnalyzer(
     private val onDrawImage: (Bitmap) -> Unit
 ) : ImageAnalysis.Analyzer {
 
     private val originalMat = Mat()
 
+    @Suppress("MagicNumber")
     override fun analyze(image: ImageProxy) {
         // Convert cameraX image to openCV mat
         val bitmap = image.toBitmap()
@@ -36,61 +39,90 @@ class CustomImageAnalyzer(
         val grayMat = Mat()
         Imgproc.cvtColor(rotated, grayMat, Imgproc.COLOR_RGBA2GRAY)
 
+        // Blurred the image
         val blurMat = Mat()
-        Imgproc.GaussianBlur(grayMat, blurMat, Size(15.0, 15.0), 0.0, 0.0)
+        Imgproc.GaussianBlur(
+            grayMat,
+            blurMat,
+            Size(15.0, 15.0),
+            0.0,
+            0.0
+        )
 
+        // Get a dynamic threshold value using the brighter pixel on the screen
         val minMaxResult = Core.minMaxLoc(blurMat)
         val thresholdMat = Mat()
-        Imgproc.threshold(blurMat, thresholdMat, minMaxResult.maxVal - 20.0, 255.0, THRESH_BINARY)
+        Imgproc.threshold(
+            blurMat,
+            thresholdMat,
+            (minMaxResult.maxVal - 10.0).coerceAtLeast(MIN_THRESHOLD),
+            255.0,
+            THRESH_BINARY
+        )
 
-        val totalPixels = thresholdMat.size().area()
-        val lightPixels = Core.countNonZero(thresholdMat)
-        val lightPercentage = lightPixels * 100 / totalPixels
 
+        // Blows are extracted easy from negative images
+        val inverseMath = Mat()
+        val keyPoints = MatOfKeyPoint()
+        val blobDetector = SimpleBlobDetector.create(BLOB_PARAMS)
+        Core.bitwise_not(thresholdMat, inverseMath)
+        blobDetector.detect(inverseMath, keyPoints)
+
+        // Draw blobs in screen
+        val blobMat = Mat()
+        Features2d.drawKeypoints(
+            rotated, keyPoints, blobMat, Scalar(0.0, 255.0, 0.0),
+            Features2d.DrawMatchesFlags_DRAW_RICH_KEYPOINTS
+        )
+
+        // Print debug data on screen
         Imgproc.rectangle(
-            rotated,
+            blobMat,
             Rect(Point(0.0, 0.0), Point(rotated.width().toDouble(), 20.0)),
             Scalar.all(50.0),
             FILLED
         )
 
         Imgproc.putText(
-            rotated,
-            "light percentage: $lightPercentage",
+            blobMat,
+            "flashlight: ${if (!keyPoints.empty()) "ON" else "OFF"}",
             Point(10.0, 10.0),
             FONT_HERSHEY_COMPLEX_SMALL,
-            0.6,
+            FONT_SCALE,
             Scalar(0.0, 255.0, 0.0)
         )
 
-        Imgproc.drawMarker(
-            rotated,
-            minMaxResult.maxLoc,
-            Scalar(0.0, 0.0, 255.0)
-        )
-
-        val contours = mutableListOf<MatOfPoint>()
-        val hierarchyMat = Mat()
-        Imgproc.findContours(
-            thresholdMat,
-            contours,
-            hierarchyMat,
-            Imgproc.RETR_LIST,
-            Imgproc.CHAIN_APPROX_SIMPLE
-        )
-        Imgproc.drawContours(rotated, contours, -1, Scalar(0.0, 255.0, 0.0))
-
         val outputBitmap = Bitmap.createBitmap(
-            rotated.width(),
-            rotated.height(),
+            blobMat.width(),
+            blobMat.height(),
             Bitmap.Config.ARGB_8888
         )
 
         // Put on the bitmap
-        Utils.matToBitmap(rotated, outputBitmap)
+        Utils.matToBitmap(blobMat, outputBitmap)
 
         image.close()
         originalMat.release()
         onDrawImage(outputBitmap)
+    }
+
+    companion object {
+        private const val FONT_SCALE = 0.6
+        private const val MIN_THRESHOLD = 158.0
+        private const val MIN_RADIUS = 5f
+        private const val MAX_RADIUS = 150f
+        private const val MIN_AREA = (Math.PI * MIN_RADIUS * MIN_RADIUS).toFloat()
+        private const val MAX_AREA = (Math.PI * MAX_RADIUS * MAX_RADIUS).toFloat()
+
+        private val BLOB_PARAMS = SimpleBlobDetector_Params().apply {
+            this._filterByArea = true
+            this._minArea = MIN_AREA
+            this._maxArea = MAX_AREA
+
+            // Circularity 1f is a perfect circle
+            this._filterByCircularity = true
+            this._minCircularity = 0.5f
+            this._maxCircularity = 1f
+        }
     }
 }
